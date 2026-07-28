fd5cbf4cf49376420fab893727497a34aa2ae249
Raman Kumar <ramankumarnke991@gmail.com>
2026-07-28 13:25:45 +0530

    Initial backend setup

--- dashboard/src/components/Positions.jsx
+++ dashboard/src/components/Positions.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { positions } from "../data/data";
 
 function Positions() {
   return (
@@ -16,6 +17,27 @@ function Positions() {
             <th>P&L</th>
             <th>Chg.</th>
           </tr>
+
+          {positions.map((stock, index) => {
+            const currVal = stock.qty * stock.price;
+            const isProfit = currVal - stock.avg * stock.qty >= 0.0;
+            const profClass = isProfit ? "profit" : "loss";
+            const dayClass = stock.isLoss ? "loss" : "profit";
+
+            return (
+              <tr key={index}>
+                <td>{stock.product}</td>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td className={profClass}>
+                  {(currVal - stock.avg * stock.qty).toFixed(2)}
+                </td>
+                <td className={dayClass}>{stock.day}</td>
+              </tr>
+            );
+          })}
         </table>
       </div>
     </>
--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { Tooltip, Grow } from "@mui/material";
+import { watchlist } from "../data/data";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
 
 function WatchList() {
   return (
@@ -11,12 +17,80 @@ function WatchList() {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
+        })}
+      </ul>
     </div>
   );
 }
+
+const WatchListItem = ({ stock }) => {
+  const [showWatchListActions, setShowWatchListActions] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    setShowWatchListActions(true);
+  };
+
+  const handleMouseLeave = (e) => {
+    setShowWatchListActions(false);
+  };
+
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <KeyboardArrowDownIcon className="down arrow" />
+          ) : (
+            <KeyboardArrowUpIcon className="up arrow" />
+          )}
+
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+      {showWatchListActions && <WatchListActions uid={stock.name} />}
+    </li>
+  );
+};
+
+const WatchListActions = ({ uid }) => {
+  return (
+    <span className="actions">
+      <Tooltip title="Buy (B)" placement="top" arrow TransitionComponent={Grow}>
+        <button className="buy">Buy</button>
+      </Tooltip>
+      <Tooltip
+        title="Sell (S)"
+        placement="top"
+        arrow
+        TransitionComponent={Grow}
+      >
+        <button className="sell">Sell</button>
+      </Tooltip>
+      <Tooltip
+        title="Analytic (A)"
+        placement="top"
+        arrow
+        TransitionComponent={Grow}
+      >
+        <button className="action">
+          <BarChartIcon className="icon" />
+        </button>
+      </Tooltip>
+      <Tooltip title="More" placement="top" arrow TransitionComponent={Grow}>
+        <button className="action">
+          <MoreHorizOutlinedIcon className="icon" />
+        </button>
+      </Tooltip>
+    </span>
+  );
+};
 
 export default WatchList;
